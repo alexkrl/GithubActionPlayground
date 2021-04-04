1b60c01b8d40e61159c3f11b41565b66272ce2e0
Alex Korolov <alexander@texel.live>
2021-04-04 14:25:23 +0300

    test#37

--- app/src/main/java/com/example/githubactionplayground/MainActivity.kt
+++ app/src/main/java/com/example/githubactionplayground/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.githubactionplayground
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("Log","onCreate: test3")
     }
 }
 
