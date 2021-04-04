2d817fe20bbf32e5c272bd4fe16d162243e8f422
Alex Korolov <alexander@texel.live>
2021-04-04 10:26:37 +0300

    test#22

--- app/src/main/java/com/example/githubactionplayground/MainActivity.kt
+++ app/src/main/java/com/example/githubactionplayground/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.githubactionplayground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +9,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+//sealed class UiState {
+//    object Idle : UiState()
+//    object Loading : UiState()
+//    data class Data(val items: List<Int>) : UiState()
+//    data class Failed(val message: String) : UiState()
+//}
+//
+//data class UiState2(
+//    val items: List<Int>,
+//    val isLoading: Boolean,
+//    val errorMessage: String? = null
+//)
+//
+//fun main() {
+//
+//    val initial = UiState2(emptyList(), false, 1)
+//
+//    val state = MutableStateFlow(initial)
+//
+//    state.value = initial.copy(isLoading = true, )
+//
+//    val newState = newState1.copy(items = listOf(1), isLoading = false)
+//
+//
+//}
+
+
+
+
+
+
