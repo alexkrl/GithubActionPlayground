430eb19baf7824cd28a4a5531bd5ace00942f256
Alex Korolov <alexander@texel.live>
2021-04-04 14:50:20 +0300

    test#41

--- app/src/main/java/com/example/githubactionplayground/MainActivity.kt
+++ app/src/main/java/com/example/githubactionplayground/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("Log","onCreate: test3")
+        Log.e("Log","onCreate: test4")
     }
 }
 
